812c0363bcc3e224971bed7d5b715025b26cf5ad
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-08-11 00:16:53 +0300

    auth

---
+++ tinkoffbank/App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthProvider } from "./app/providers/AuthProvider";
+
+export default function App() {
+    return <AuthProvider>
+          
+        </AuthProvider>
+}
---
+++ tinkoffbank/app/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    signOut,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth() //или предать туда app?
+
+export const register = (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password);
+
+export const login = (email: string, password: string) => signInWithEmailAndPassword(auth, email, password);
+
+export const logout = () => signOut(auth)
+
+export const db = getFirestore()
