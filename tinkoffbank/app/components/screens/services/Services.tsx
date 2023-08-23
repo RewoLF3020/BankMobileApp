6b8ab23c11fc53191b470b4bb60483ac309ebce4
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-08-23 20:54:30 +0300

    services added

--- tinkoffbank/app/components/screens/services/Services.tsx
+++ tinkoffbank/app/components/screens/services/Services.tsx
@@ -1,11 +1,20 @@
 import { View, Text } from "react-native";
 import React, { FC } from "react";
+import tw from "twrnc";
+import Layout from "../../layout/Layout";
+import { services } from "./data";
+import ServiceItem from "./item/ServiceItem";
 
 const Services: FC = () => {
     return (
-        <View>
-            <Text>Services</Text>
-        </View>
+        <Layout>
+            <Text style={tw`text-center text-lg mb-6`}>
+                Moscow
+            </Text>
+            <View style={tw`flex-row flex-wrap justify-center`}>
+                {services.map(service => <ServiceItem key={service.title} service={service} />)}
+            </View>
+        </Layout>
     );
 };
 
